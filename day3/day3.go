4686b0303e9ea703e7aac4635dc61ce86b2272f4
gjrud <f.basili@outlook.com>
2024-03-19 21:11:46 +0100

    aoc2023 day3 Part 2

--- day3/day3.go
+++ day3/day3.go
@@ -8,36 +8,53 @@ import (
 	"unicode"
 )
 
+type part struct {
+	num  int
+	kind rune
+	x, y int
+}
+
+type gear struct {
+	num1, num2 int
+	x, y       int
+}
+
 func main() {
 	input, err := utils.ReadInputToStringArray("input.txt", "\r\n")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	partNums, err := getPartNumbers(input)
+	parts, err := getPartNumbers(input)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	sum := 0
-	for _, pn := range partNums {
-		sum += pn
+	for _, pn := range parts {
+		sum += pn.num
 	}
 	fmt.Println(sum)
+
+	gears := getGears(parts)
+	sumGears := 0
+	for _, g := range gears {
+		sumGears += g.num1 * g.num2
+	}
+	fmt.Println(sumGears)
 }
 
-func getPartNumbers(input []string) ([]int, error) {
-	partNums := make([]int, 0)
+func getPartNumbers(input []string) ([]part, error) {
+	partNums := make([]part, 0)
 	for y, line := range input {
 		for x := 0; x < len(line); x++ {
 			v := rune(line[x])
 			if unicode.IsDigit(v) {
-				newPartNum, length, err := getNumbers(input, x, y)
+				newPn, length, err := getNumbers(input, x, y)
 				if err != nil {
-					return make([]int, 0), err
+					return make([]part, 0), err
 				}
-				if newPartNum != 0 {
-					partNums = append(partNums, newPartNum)
+				if newPn != (part{}) {
+					partNums = append(partNums, newPn)
 				}
 
 				x += length - 1
@@ -47,9 +64,9 @@ func getPartNumbers(input []string) ([]int, error) {
 	return partNums, nil
 }
 
-func getNumbers(input []string, xStart, yStart int) (int, int, error) {
+func getNumbers(input []string, col, row int) (part, int, error) {
 	strNum := ""
-	for _, v := range input[yStart][xStart:] {
+	for _, v := range input[row][col:] {
 		if unicode.IsDigit(v) {
 			strNum += string(v)
 		} else {
@@ -57,16 +74,47 @@ func getNumbers(input []string, xStart, yStart int) (int, int, error) {
 		}
 	}
 
-	for _, line := range input[utils.Max(yStart-1, 0):utils.Min(yStart+2, len(input))] {
-		for _, v := range line[utils.Max(xStart-1, 0):utils.Min(xStart+len(strNum)+1, len(line))] {
+	yStart, yEnd := utils.Max(row-1, 0), utils.Min(row+2, len(input))
+	xStart := utils.Max(col-1, 0)
+
+	for y, line := range input[yStart:yEnd] {
+		xEnd := utils.Min(col+len(strNum)+1, len(line))
+		for x, v := range line[xStart:xEnd] {
 			if !unicode.IsDigit(v) && v != '.' {
-				partNum, err := strconv.Atoi(strNum)
+				num, err := strconv.Atoi(strNum)
 				if err != nil {
-					return 0, 0, err
+					return part{}, 0, err
 				}
-				return partNum, len(strNum), nil
+				pn := part{num, v, x + xStart, y + yStart}
+				return pn, len(strNum), nil
 			}
 		}
 	}
-	return 0, len(strNum), nil
+	return part{}, len(strNum), nil
+}
+
+func getGears(parts []part) []gear {
+	gears := make([]gear, 0)
+	for i, p1 := range parts[:len(parts)-1] {
+		if p1.kind != '*' {
+			continue
+		}
+
+		g := gear{}
+		for _, p2 := range parts[i+1:] {
+			if p2.kind != '*' {
+				continue
+			}
+
+			if p1.x == p2.x && p1.y == p2.y && g == (gear{}) {
+				g = gear{p1.num, p2.num, p1.x, p2.y}
+			} else if g != (gear{}) {
+				break
+			}
+		}
+		if g != (gear{}) {
+			gears = append(gears, g)
+		}
+	}
+	return gears
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -23,23 +23,53 @@ func TestGetPartNumbers(t *testing.T) {
 	expectedSlice := []int{35, 467, 592, 598, 617, 633, 664, 755}
 	expectedSum := 4361
 
-	partNumbers, err := getPartNumbers(input)
+	parts, err := getPartNumbers(input)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sort.Slice(partNumbers, func(i, j int) bool {
-		return partNumbers[i] < partNumbers[j]
+	sort.Slice(parts, func(i, j int) bool {
+		return parts[i].num < parts[j].num
 	})
 
-	if !slices.Equal(partNumbers, expectedSlice) {
-		log.Fatalf("NOT EQUAL\n%v", partNumbers)
+	partNums := make([]int, len(parts))
+	for i, p := range parts {
+		partNums[i] = p.num
+	}
+
+	if !slices.Equal(partNums, expectedSlice) {
+		log.Fatalf("NOT EQUAL\n%v", parts)
 	}
 
 	sum := 0
-	for _, pn := range partNumbers {
+	for _, pn := range partNums {
 		sum += pn
 	}
+	if sum != expectedSum {
+		log.Fatalf("NOT EQUAL\n%v", sum)
+	}
+}
+
+func TestGetGears(t *testing.T) {
+	parts := []part{
+		{467, 42, 3, 1},
+		{35, 42, 3, 1},
+		{633, 35, 6, 3},
+		{617, 42, 3, 4},
+		{592, 43, 5, 5},
+		{755, 42, 5, 8},
+		{664, 36, 3, 8},
+		{598, 42, 5, 8},
+	}
+	expectedSum := 467835
+
+	gears := getGears(parts)
+
+	sum := 0
+	for _, g := range gears {
+		sum += g.num1 * g.num2
+	}
+
 	if sum != expectedSum {
 		log.Fatalf("NOT EQUAL\n%v", sum)
 	}
