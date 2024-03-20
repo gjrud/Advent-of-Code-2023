4067510f890383fc2d29aa3cb0e11b86a3e39921
gjrud <f.basili@outlook.com>
2024-03-20 13:20:10 +0100

    day4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -11,9 +11,10 @@ import (
 type card struct {
 	winNums []int
 	nums    []int
+	copies  int
 }
 
-func (c card) worth() int {
+func (c card) winCount() int {
 	winCount := 0
 	for _, w := range c.winNums {
 		for _, n := range c.nums {
@@ -22,12 +23,7 @@ func (c card) worth() int {
 			}
 		}
 	}
-
-	if winCount == 0 {
-		return 0
-	} else {
-		return 1 << (winCount - 1)
-	}
+	return winCount
 }
 
 func main() {
@@ -41,11 +37,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sum := 0
-	for _, c := range cards {
-		sum += c.worth()
+	sumWorth := 0
+	sumCopies := 0
+	for i, c := range cards {
+		winCount := c.winCount()
+		for j := 1; j < len(cards) && j <= winCount; j++ {
+			cards[i+j].copies += c.copies
+		}
+		if winCount > 0 {
+			sumWorth += 1 << (winCount - 1)
+		}
+		sumCopies += c.copies
 	}
-	fmt.Println(sum)
+	fmt.Println(sumWorth)
+	fmt.Println(sumCopies)
 }
 
 func parseInput(input []string) ([]card, error) {
@@ -69,7 +74,7 @@ func parseInput(input []string) ([]card, error) {
 		if err != nil {
 			return make([]card, 0), err
 		}
-		cards = append(cards, card{winNums, nums})
+		cards = append(cards, card{winNums, nums, 1})
 	}
 	return cards, nil
 }
--- day4/say4_test.go
+++ day4/say4_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 var expectedCards = []card{
-	{[]int{41, 48, 83, 86, 17}, []int{83, 86, 6, 31, 17, 9, 48, 53}},
-	{[]int{13, 32, 20, 16, 61}, []int{61, 30, 68, 82, 17, 32, 24, 19}},
-	{[]int{1, 21, 53, 59, 44}, []int{69, 82, 63, 72, 16, 21, 14, 1}},
-	{[]int{41, 92, 73, 84, 69}, []int{59, 84, 76, 51, 58, 5, 54, 83}},
-	{[]int{87, 83, 26, 28, 32}, []int{88, 30, 70, 12, 93, 22, 82, 36}},
-	{[]int{31, 18, 13, 56, 72}, []int{74, 77, 10, 23, 35, 67, 36, 11}},
+	{[]int{41, 48, 83, 86, 17}, []int{83, 86, 6, 31, 17, 9, 48, 53}, 1},
+	{[]int{13, 32, 20, 16, 61}, []int{61, 30, 68, 82, 17, 32, 24, 19}, 1},
+	{[]int{1, 21, 53, 59, 44}, []int{69, 82, 63, 72, 16, 21, 14, 1}, 1},
+	{[]int{41, 92, 73, 84, 69}, []int{59, 84, 76, 51, 58, 5, 54, 83}, 1},
+	{[]int{87, 83, 26, 28, 32}, []int{88, 30, 70, 12, 93, 22, 82, 36}, 1},
+	{[]int{31, 18, 13, 56, 72}, []int{74, 77, 10, 23, 35, 67, 36, 11}, 1},
 }
 
 var isCardEqual = func(e1, e2 card) bool {
@@ -44,11 +44,33 @@ func TestParseInput(t *testing.T) {
 
 func TestWorth(t *testing.T) {
 	expectedWorth := 13
+
 	sum := 0
 	for _, c := range expectedCards {
-		sum += c.worth()
+		wincount := c.winCount()
+		if wincount > 0 {
+			sum += 1 << (wincount - 1)
+		}
 	}
 	if sum != expectedWorth {
 		t.Fatalf("MISMATCH\n%d\n%d", expectedWorth, sum)
+	}
+}
+
+func TestCopies(t *testing.T) {
+	cards := make([]card, len(expectedCards))
+	copy(cards, expectedCards)
+	expectedCopies := 30
+
+	sum := 0
+	for i, c := range cards {
+		winCount := c.winCount()
+		for j := 1; j < len(cards) && j <= winCount; j++ {
+			cards[i+j].copies += c.copies
+		}
+		sum += c.copies
+	}
+	if sum != expectedCopies {
+		t.Fatalf("MISMATCH\n%d\n%d", expectedCopies, sum)
 	}
 }
