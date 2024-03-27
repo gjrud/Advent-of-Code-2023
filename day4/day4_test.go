e8c64addb3215fcac89af411f6e2f4fbd7f9e882
Federico Basili <f.basili@outlook.com>
2024-03-27 14:17:57 +0100

    moved single vars in var()

--- day4/day4_test.go
+++ day4/day4_test.go
@@ -5,22 +5,24 @@ import (
 	"testing"
 )
 
-var expectedCards = []card{
-	{[]int{41, 48, 83, 86, 17}, []int{83, 86, 6, 31, 17, 9, 48, 53}, 1},
-	{[]int{13, 32, 20, 16, 61}, []int{61, 30, 68, 82, 17, 32, 24, 19}, 1},
-	{[]int{1, 21, 53, 59, 44}, []int{69, 82, 63, 72, 16, 21, 14, 1}, 1},
-	{[]int{41, 92, 73, 84, 69}, []int{59, 84, 76, 51, 58, 5, 54, 83}, 1},
-	{[]int{87, 83, 26, 28, 32}, []int{88, 30, 70, 12, 93, 22, 82, 36}, 1},
-	{[]int{31, 18, 13, 56, 72}, []int{74, 77, 10, 23, 35, 67, 36, 11}, 1},
-}
-
-var isCardEqual = func(e1, e2 card) bool {
-	equal := slices.Equal(e1.winNums, e2.winNums)
-	if equal {
-		equal = slices.Equal(e1.nums, e2.nums)
+var (
+	expectedCards = []card{
+		{[]int{41, 48, 83, 86, 17}, []int{83, 86, 6, 31, 17, 9, 48, 53}, 1},
+		{[]int{13, 32, 20, 16, 61}, []int{61, 30, 68, 82, 17, 32, 24, 19}, 1},
+		{[]int{1, 21, 53, 59, 44}, []int{69, 82, 63, 72, 16, 21, 14, 1}, 1},
+		{[]int{41, 92, 73, 84, 69}, []int{59, 84, 76, 51, 58, 5, 54, 83}, 1},
+		{[]int{87, 83, 26, 28, 32}, []int{88, 30, 70, 12, 93, 22, 82, 36}, 1},
+		{[]int{31, 18, 13, 56, 72}, []int{74, 77, 10, 23, 35, 67, 36, 11}, 1},
 	}
-	return equal
-}
+
+	isCardEqual = func(e1, e2 card) bool {
+		equal := slices.Equal(e1.winNums, e2.winNums)
+		if equal {
+			equal = slices.Equal(e1.nums, e2.nums)
+		}
+		return equal
+	}
+)
 
 func TestParseInput(t *testing.T) {
 	input := []string{
